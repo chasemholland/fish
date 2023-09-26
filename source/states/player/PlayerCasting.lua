a65c5e775fb85a3d41ce07cb5354db9794fef40a
chasemholland <127550059+chasemholland@users.noreply.github.com>
2023-09-26 13:22:47 -0700

    Update PlayerCasting.lua

--- source/states/player/PlayerCasting.lua
+++ source/states/player/PlayerCasting.lua
@@ -65,27 +65,85 @@ function PlayerCasting:Cast()
     self.player.power = math.floor(self.player.timer)
     self.player.timer = 0
 
-    -- only allow casting into water
-    if self.player.frame == PLAYER_CAST_LEFT then
-        self.player.frame = PLAYER_FISH_LEFT
-        self.player:changeState('idle')
-    elseif self.player.frame == PLAYER_CAST_RIGHT then
-        self.player.frame = PLAYER_FISH_RIGHT
-        self.player:changeState('idle')
-
-    elseif self.player.frame == PLAYER_CAST_UP then
-        self.player.frame = PLAYER_FISH_UP
-        self.player:changeState('idle')
-
-    elseif self.player.frame == PLAYER_CAST_DOWN then
-        self.player.frame = PLAYER_FISH_DOWN
-
-        -- check if cast was on a water tile
-        if ((self.player.y + POLE_TIP_DOWN[2]) + self.player.power) <= WATER_TOP then
+    if self.player.area == 'start' then
+        -- only allow casting into water
+        if self.player.frame == PLAYER_CAST_LEFT then
+            self.player.frame = PLAYER_FISH_LEFT
             self.player:changeState('idle')
-        else
-            self.player:changeState('fishing')
+        elseif self.player.frame == PLAYER_CAST_RIGHT then
+            self.player.frame = PLAYER_FISH_RIGHT
+            self.player:changeState('idle')
+
+        elseif self.player.frame == PLAYER_CAST_UP then
+            self.player.frame = PLAYER_FISH_UP
+            self.player:changeState('idle')
+
+        elseif self.player.frame == PLAYER_CAST_DOWN then
+            self.player.frame = PLAYER_FISH_DOWN
+
+            -- check if cast was on a water tile
+            if ((self.player.y + POLE_TIP_DOWN[2]) + self.player.power) <= WATER_TOP then
+                self.player:changeState('idle')
+            else
+                self.player:changeState('fishing')
+            end
+        end
+    elseif self.player.area == 'beach' then
+        -- only allow casting into water
+        if self.player.frame == PLAYER_CAST_LEFT then
+            self.player.frame = PLAYER_FISH_LEFT
+            self.player:changeState('idle')
+        elseif self.player.frame == PLAYER_CAST_RIGHT then
+            self.player.frame = PLAYER_FISH_RIGHT
+
+            -- check if cast was on a water tile
+            if ((self.player.x + POLE_TIP_RIGHT[1]) + self.player.power) <= GAME_WIDTH / 2 + 16 then
+                self.player:changeState('idle')
+            else
+                -- check if player has high enough lure
+                if self.player.inventory['lure'] ~= 'basic' and self.player.inventory['lure'] ~= 'novice'then
+                    self.player:changeState('fishing')
+                else
+                    self.player:changeState('idle')
+                end
+            end
+
+        elseif self.player.frame == PLAYER_CAST_UP then
+            self.player.frame = PLAYER_FISH_UP
+            self.player:changeState('idle')
+
+        elseif self.player.frame == PLAYER_CAST_DOWN then
+            self.player.frame = PLAYER_FISH_DOWN
+            self.player:changeState('idle')
+        end
+    elseif self.player.area == 'river' then
+        -- only allow casting into water
+        if self.player.frame == PLAYER_CAST_LEFT then
+            self.player.frame = PLAYER_FISH_LEFT
+
+            -- check if cast was on water tile
+            if ((self.player.x - POLE_TIP_LEFT[1]) - self.player.power) >= GAME_WIDTH / 2 or ((self.player.x - POLE_TIP_LEFT[1]) - self.player.power) <= 96 then
+                self.player:changeState('idle')
+            else
+                -- check if player has high enough lure
+                if self.player.inventory['lure'] ~= 'basic' then
+                    self.player:changeState('fishing')
+                else
+                    self.player:changeState('idle')
+                end
+            end
+
+        elseif self.player.frame == PLAYER_CAST_RIGHT then
+            self.player.frame = PLAYER_FISH_RIGHT
+            self.player:changeState('idle')
+
+        elseif self.player.frame == PLAYER_CAST_UP then
+            self.player.frame = PLAYER_FISH_UP
+            self.player:changeState('idle')
+
+        elseif self.player.frame == PLAYER_CAST_DOWN then
+            self.player.frame = PLAYER_FISH_DOWN
+            self.player:changeState('idle')
         end
     end
-
 end

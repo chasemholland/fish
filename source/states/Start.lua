f5d3b6330899b16ba6b5cb8abbfd279e232c1bf4
chasemholland <127550059+chasemholland@users.noreply.github.com>
2023-09-27 11:44:01 -0700

    Update Start.lua

--- source/states/Start.lua
+++ source/states/Start.lua
@@ -6,6 +6,11 @@ Start = Class{__includes = Base}
 
 function Start:init()
 
+    -- player
+    self.player = Player()
+
+    -- skip tutorial
+    self.skip = false
 
 end
 
@@ -15,10 +20,28 @@ function Start:update(dt)
     if love.keyboard.pressed('escape') then
         love.event.quit()
     end
+
+    -- check box to skip tutorial
+    if love.mouse.pressed(1) and mouseX >= GAME_WIDTH / 2 - 120 and mouseX <= GAME_WIDTH / 2 - 104 
+    and mouseY >= GAME_HEIGHT / 2 + 46 and mouseY <= GAME_HEIGHT / 2 + 62 then
+        if not self.skip then
+            self.skip = true
+            for i = 1, 6 do
+                self.player.tutorial[i] = true
+            end
+        else
+            self.skip = false
+            for i = 1, 6 do
+                self.player.tutorial[i] = false
+            end
+        end
+    end
  
     -- change to play state
     if love.keyboard.pressed('enter') or love.keyboard.pressed('return') then
-        gameSM:change('play')
+        gameSM:change('play', {
+            player = self.player
+        })
     end
 
 end
@@ -39,11 +62,25 @@ function Start:render()
 
     love.graphics.setFont(Fonts['md'])
     love.graphics.setColor(10 / 255, 40 / 255, 140 / 255, 1)
-    love.graphics.printf("FISH-LANDIA", GAME_WIDTH / 2 - 184, GAME_HEIGHT / 2 - 44, 600)
+    love.graphics.printf("FISH-LANDIA", GAME_WIDTH / 2 - 184, GAME_HEIGHT / 2 - 64, 600)
 
     love.graphics.setColor(0, 0, 0, 1)
     love.graphics.setFont(Fonts['sm'])
-    love.graphics.printf("PRESS ENTER TO START!", GAME_WIDTH / 2 - 170, GAME_HEIGHT / 2 + 44, 600)
+    love.graphics.printf("PRESS ENTER TO START!", GAME_WIDTH / 2 - 170, GAME_HEIGHT / 2 , 600)
+
+    -- skip tutorial
+    love.graphics.printf("SKIP TUTORIAL", GAME_WIDTH / 2 - 100, GAME_HEIGHT / 2 + 44, 600)
+    if self.skip then
+        love.graphics.setColor(1, 0, 0, 1)
+        love.graphics.setLineWidth(3)
+        love.graphics.rectangle('fill', GAME_WIDTH / 2 - 120, GAME_HEIGHT / 2 + 46, 16, 16)
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.rectangle('line', GAME_WIDTH / 2 - 120, GAME_HEIGHT / 2 + 46, 16, 16)
+    else
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.setLineWidth(3)
+        love.graphics.rectangle('line', GAME_WIDTH / 2 - 120, GAME_HEIGHT / 2 + 46, 16, 16)
+    end
     
 
 end
